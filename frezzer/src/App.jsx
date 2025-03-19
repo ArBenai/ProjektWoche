aeee62419bf5269b30b02700bbcd07130b2464e4
ArBenai <arben.musa@dci-student.org>
2025-03-19 13:56:09 +0100

    add startscreen

--- frezzer/src/App.jsx
+++ frezzer/src/App.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import StartScreen from './components/StartScreen';
 import InsideFridge from './components/InsideFridge';
-import './App.css';
+import "./App.css";
+
 
 const App = () => {
+  // Zustand für die Anzeige der Komponenten
+  const [isStarted, setIsStarted] = useState(false);
+
+  // Funktion, um die Anzeige zu wechseln
+  const handleStart = () => {
+    setIsStarted(true);
+  };
+
   return (
     <>
-    <div className="App">
-      <InsideFridge />
-    </div>
+      {!isStarted ? (
+        <StartScreen onStart={handleStart} />
+      ) : (
+        <InsideFridge />
+      )}
     </>
   );
-}
+};
 
 export default App;
