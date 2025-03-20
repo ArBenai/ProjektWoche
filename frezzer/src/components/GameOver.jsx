255549d85c762661f8a07c2530521ac49f75ea8c
ArBenai <arben.musa@dci-student.org>
2025-03-20 15:35:25 +0100

    style: enhance GameOver component layout and styling for improved user experience

--- frezzer/src/components/GameOver.jsx
+++ frezzer/src/components/GameOver.jsx
@@ -4,10 +4,12 @@ import '../styles/GameOver.css';
 const GameOver = ({ score, onRestart, message }) => {
   return (
     <div className="game-over">
+      <div className='game-over-content'>
       <h1>Game Over</h1>
       <p>{message}</p>
       <p>Dein Score: {score}</p>
       <button onClick={onRestart}>Neustart</button>
+    </div>
     </div>
   );
 };
